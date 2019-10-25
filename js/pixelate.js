91028687afeab1b78dc72ec34437147a34bd003e
Reavershark <jonasty1999@gmail.com>
2019-10-25 16:09:32 +0200

    Multiple cursors

--- js/pixelate.js
+++ js/pixelate.js
@@ -1,36 +1,44 @@
 let canvas = document.getElementById("displayCanvas");
 let display = new Display(canvas);
-let cursor = new Cursor(display);
 
-let controller = new BleController();
+let players = [];
+
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("connect").addEventListener("click", () => {
-        controller.connect()
+        players.push(new Player());
+    });
+});
+
+class Player {
+    constructor() {
+        this.controller = new BleController();
+        this.cursor = new Cursor(display);
+
+        this.controller.connect()
         .then(() => {
             console.log("Connected to BLE controller");
-            cursor.change_color({r:0, g:0, b:200});
-            controller.addEventListener('touch', (event) => {
+            this.controller.addEventListener('touch', (event) => {
                 switch(event.touchedKey) {
-                    case 'R': cursor.move_right(); break;
-                    case 'L': cursor.move_left(); break;
-                    case 'U': cursor.move_up(); break;
-                    case 'D': cursor.move_down(); break;
+                    case 'R': this.cursor.move_right(); break;
+                    case 'L': this.cursor.move_left(); break;
+                    case 'U': this.cursor.move_up(); break;
+                    case 'D': this.cursor.move_down(); break;
                     case 'X':
-                        cursor.change_color({r:200, g:0, b:0});
-                        cursor.colorize();
+                        this.cursor.change_color({r:200, g:0, b:0});
+                        this.cursor.colorize();
                         break;
                     case 'A':
-                        cursor.change_color({r:0, g:200, b:0});
-                        cursor.colorize();
+                        this.cursor.change_color({r:0, g:200, b:0});
+                        this.cursor.colorize();
                         break;
                     case 'B':
-                        cursor.change_color({r:0, g:0, b:200});
-                        cursor.colorize();
+                        this.cursor.change_color({r:0, g:0, b:200});
+                        this.cursor.colorize();
                         break;
                 }
             });
         }).catch((error) => {
             console.log(error);
         });
-    });
-});
+    }
+}
